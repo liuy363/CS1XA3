712aae5af24172565f25b401bd5a3b910a533e2e
Cynthia <liuy363@mcmaster.ca>
2019-03-03 17:32:34 +0000

    add lab06

---
+++ Lab06/src/Lab06.elm
@@ -0,0 +1,60 @@
+import Browser
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { string1 : String
+  , string2 : String
+  }
+
+
+init : Model
+init =
+  { string1 = "" 
+  , string2 = "" 
+  }
+
+
+
+-- UPDATE
+
+
+type Msg
+  = String1 String
+  | String2 String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    String1 string1 ->
+      { model | string1 = string1 }
+    String2 string2 ->
+      { model | string2 = string2 }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "String 1", value model.string1, onInput String1 ] [] 
+    , input [ placeholder "String 2", value model.string2, onInput String2 ] [] 
+    , div [] [ text (model.string1) , text ":" ,text (model.string2) ]
+    ]
