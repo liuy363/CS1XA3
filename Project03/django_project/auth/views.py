15c02a8d43f23b1ab9815b7f68d8da030867d7bb
Cynthia <liuy363@mcmaster.ca>
2019-04-16 00:55:44 +0000

    initial commit

---
+++ Project03/django_project/auth/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+import json
+
+from .models import UserInfo
+
+def session_incr(request):
+    """increments a counter held in the current session"""
+
+    i = request.session.get('counter',0)
+    request.session['counter'] = i+1
+
+    return HttpResponse("Counter = " + str(request.session['counter']))
+
+def session_get(request):
+    return HttpResponse("Counter = " + str(request.session['counter']))
+
+def add_user(request):
+    """recieves a json request { 'username' : 'val0', 'password' : 'val1' } and saves it
+       it to the database using the django User Model
+       Assumes success and returns an empty Http Response"""
+
+    json_req = json.loads(request.body)
+    uname = json_req.get('username','')
+    passw = json_req.get('password','')
+
+    if uname != '':
+        user = User.objects.create_user(username=uname,
+                                        password=passw)
+
+        login(request,user)
+        return HttpResponse('LoggedIn')
+
+    else:
+        return HttpResponse('LoggedOut')
+
+
+def login_user(request):
+    """recieves a json request { 'username' : 'val0' : 'password' : 'val1' } and
+       authenticates and loggs in the user upon success """
+
+    json_req = json.loads(request.body)
+    uname = json_req.get('username','')
+    passw = json_req.get('password','')
+
+    user = authenticate(request,username=uname,password=passw)
+    if user is not None:
+        login(request,user)
+        return HttpResponse("LoggedIn")
+    else:
+        return HttpResponse('LoginFailed')
+
+def user_info(request):
+    """serves content that is only available to a logged in user"""
+
+    if not request.user.is_authenticated:
+        return HttpResponse("LoggedOut")
+    else:
+        # do something only a logged in user can do
+        return HttpResponse("Hi " + request.user.first_name)
